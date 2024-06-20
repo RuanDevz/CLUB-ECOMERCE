444cb15e87c1e654302f48f9cf0577d73dd17c40
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-19 23:29:52 -0300

    feat: add login auth with token acess and verify user logged

--- BACKEND/routes/user.js
+++ BACKEND/routes/user.js
@@ -2,6 +2,11 @@ const express  = require('express')
 const router = express.Router()
 const {User} = require('../models')
 const bcrypt = require('bcrypt')
+const {sign} = require('jsonwebtoken')
+const Authmiddleware = require('../Middleware/Auth')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 
 
@@ -28,6 +33,31 @@ router.post('/register', async (req,res) =>{
 
     res.status(201).json(createnewuser)
 })
+
+router.post('/login', async (req,res) =>{
+    const {email, password} = req.body
+
+    const user = await User.findOne({where: {email}})
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ error: "Credenciais incorretas!" });
+    }
+
+    const accesstoken = sign({email: user.email, id: user.id}, process.env.TOKEN_VERIFY_ACCESS)
+
+    res.json({token: accesstoken})
+})
+router.get('/dashboard', Authmiddleware, async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const user = await User.findByPk(userId);
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Erro interno do servidor" })
+    }
+});
 
 
 
