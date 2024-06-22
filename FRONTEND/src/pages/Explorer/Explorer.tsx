a8e81c2cb9a7e6896143253d9ae74e2e05c99da2
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-22 00:42:24 -0300

    feat: add loading on explorer page

--- FRONTEND/src/pages/Explorer/Explorer.tsx
+++ FRONTEND/src/pages/Explorer/Explorer.tsx
@@ -1,33 +1,42 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import Header from '../../components/Header/Header'
 import axios from 'axios'
 import productsProps from '../../types/Product.types';
 import Backpage from '../Backpage/Backpage';
 import HeaderLogged from '../../components/HeaderLogged/HeaderLogged';
-import Context from '../../Context/Context';
+import Spacer from '../../components/Spacer/Spacer';
 
 
 const Explorer = () => {
 
   const [products, setProducts] = useState<productsProps[]>([])
 
-  const {accessToken}  = useContext(Context)
+  const userlogged = sessionStorage.getItem('token')
+  const Googlelogged = sessionStorage.getItem('Googletoken')
+
+  const [loading, setLoading] = useState<boolean>(false)
 
 
   useEffect(() =>{
+    setLoading(true)
     axios.get(`${import.meta.env.VITE_API_URL}/products`)
     .then((response) =>{
       setProducts(response.data)
+      setLoading(false)
     })
   },[])
 
   return (
     <div>
-      {accessToken ? <HeaderLogged/> : <Header/>}
+      {userlogged || Googlelogged  ? <HeaderLogged/> : <Header/>}
       <main>
         <Backpage>Explorer</Backpage>
       <section className='max-w-default flex justify-around items-center gap-10 flex-wrap mx-auto'>
-        {products.map((product) =>(
+        {loading ? (
+          <Spacer/>
+        ):(
+          <>
+          {products.map((product) =>(
           <div key={product.id}>
             <img className='w-productwidth h-productheight rounded-xl font-primary' src={product.imageUrl} alt={product.name} />
             <div className='flex justify-between py-2'>
@@ -36,6 +45,8 @@ const Explorer = () => {
             </div>
           </div>
         ))}
+          </>
+        )}
       </section>
       </main>
     </div>
