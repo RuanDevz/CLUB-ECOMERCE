6233fc07face6242c816e4bb164ac5ab551c5d3b
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-22 00:38:55 -0300

    feat: add a loadingcomponent

--- FRONTEND/src/components/Categories/Categories.tsx
+++ FRONTEND/src/components/Categories/Categories.tsx
@@ -3,15 +3,20 @@ import axios from 'axios';
 import './Categories.css'; 
 import CategoryItem from '../CategoryItem/CategoryItem'; 
 import Category from '../../types/Categories.types';
+import Loader from '../Loader/Loader';
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([]);
 
+  const [loading, setLoading] = useState<boolean>(false)
+
   const fetchCategories = async () => {
     try {
+      setLoading(true)
       const response = await axios.get(`${import.meta.env.VITE_API_URL}/categories`);
       console.log(response.data);
       setCategories(response.data);
+      setLoading(false)
     } catch (error) {
       console.error("There was an error fetching the categories!", error);
     }
@@ -24,11 +29,19 @@ const Categories = () => {
   return (
     <div className='categories-container'>
       <div className='categories-content'>
-        {categories.map((category) =>(
+        {loading ? (
+          <div className='flex justify-center items-center h-screen w-screen'>
+            <Loader/>
+          </div>
+        ):(
+          <>
+          {categories.map((category) =>(
           <div key={category.id}>
             <CategoryItem category={category} />
           </div>
         ))}
+          </>
+        )}
       </div>
     </div>
   );
---
+++ FRONTEND/src/components/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loader.css'
+
+const Loader = () => {
+  return (
+    <div>
+      <div className="loader mb-40">
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+</div>
+    </div>
+  )
+}
+
+export default Loader
