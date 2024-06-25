1bee08cb5e8b883f21051141c21f556423ee4415
RuanDevz <Ruanbatista1509@outlook.com>
2024-06-25 19:57:22 -0300

    feat: add a animate transition

--- FRONTEND/src/components/Cart/Cart.tsx
+++ FRONTEND/src/components/Cart/Cart.tsx
@@ -6,6 +6,7 @@ import { FaXmark } from 'react-icons/fa6';
 import { IoMdAdd } from 'react-icons/io';
 import Context from '../../context/Context';
 import { useNavigate } from 'react-router-dom';
+import './cart.css'
 
 
 interface Product {
@@ -80,7 +81,7 @@ const Cart = () => {
 
   return (
     <>
-    <div onClick={handlecartitem} id='modalcart' className={`fixed h-screen transition-all duration-300 ease-in-out w-screen right-0 bottom-0 left-0 top-0 bg-cartbg bg-opacity-10 inset-0 backdrop-blur-sm flex justify-end text-dark mt-20 ${showCartItem ? '' : 'hidden'}`}>
+    <div onClick={handlecartitem} id='modalcart' className={`fixed h-screen animated transition-all duration-300 ease-in-out w-screen right-0 bottom-0 left-0 top-0 bg-cartbg bg-opacity-10 inset-0 backdrop-blur-sm flex justify-end text-dark mt-20 ${showCartItem ? '' : 'hidden'}`}>
       <div ref={cartRef} className={`h-full bg-white p-5 w-cart overflow-y-scroll`}>
         <p className='mb-4 font-semibold text-2xl'>Seu carrinho</p>
         <div className='h-[380px] lg:h-[630px] overflow-y-auto'>
